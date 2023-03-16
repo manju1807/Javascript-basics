95d8228d672df20fe63f8755bfe2d7230fbc8df7
manjunath R <rmanjunath7692@gmail.com>
2023-03-16 23:26:37 +0530

    add console.js

---
+++ section1-variables datatypes etc/console.js
@@ -0,0 +1,20 @@
+// different console options in js
+
+
+console.log("Hello World");
+console.error("This is an error");
+console.warn("This is a warning");
+const x= 100;
+console.log(x);
+
+console.table({name: 'manja', email:'rmanjunath7692@gmail.com'})
+
+console.group('simple');
+console.log('hello');
+console.error('alert');
+console.warn('warning');
+console.groupEnd('simple');
+
+const styles="padding:10px; background-color:white;color:green;"
+
+console.log('%c Hello World',styles);
