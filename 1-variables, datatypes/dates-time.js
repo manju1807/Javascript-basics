4965017ca189f13b836de3207bfc7ae0612346bd
manjunath R <rmanjunath7692@gmail.com>
2023-03-21 14:53:22 +0530

    update

---
+++ 1-variables, datatypes/dates-time.js
@@ -0,0 +1,104 @@
+// date time in js
+
+// Date object
+let date = new Date();
+console.log(date);
+
+// get date
+let date1 = new Date();
+console.log(date1.getDate());
+
+// get month
+let date2 = new Date();
+console.log(date2.getMonth());
+
+// get year
+let date3 = new Date();
+console.log(date3.getFullYear());
+
+// get day
+let date4 = new Date();
+console.log(date4.getDay());
+
+// get hours
+let date5 = new Date();
+console.log(date5.getHours());
+
+// get minutes
+let date6 = new Date();
+console.log(date6.getMinutes());
+
+// get seconds
+let date7 = new Date();
+
+
+// get milliseconds
+let date8 = new Date();
+
+
+// get time
+let date9 = new Date();
+
+
+
+// set date
+let date10 = new Date();
+date10.setDate(10);
+console.log(date10);
+
+// set month
+let date11 = new Date();
+date11.setMonth(10);
+console.log(date11);
+
+// set year
+let date12 = new Date();
+date12.setFullYear(2020);
+console.log(date12);
+
+// set hours
+let date13 = new Date();
+date13.setHours(10);
+console.log(date13);
+
+// set minutes
+let date14 = new Date();
+date14.setMinutes(10);
+console.log(date14);
+
+// set seconds
+let date15 = new Date();
+date15.setSeconds(10);
+console.log(date15);
+
+// set milliseconds
+let date16 = new Date();
+date16.setMilliseconds(10);
+console.log(date16);
+
+// set time
+let date17 = new Date();
+date17.setTime(10);
+console.log(date17);
+
+// toDateString
+let date18 = new Date();
+console.log(date18.toDateString());
+
+
+// toTimeString
+let date19 = new Date();
+console.log(date19.toTimeString());
+
+
+// toLocaleDateString
+let date20 = new Date();
+console.log(date20.toLocaleDateString());
+
+// toLocaleTimeString
+let date21 = new Date();
+console.log(date21.toLocaleTimeString());
+
+
+
+
