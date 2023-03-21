2e364faaf4aaba9f74254519e954fe55f560572b
manjunath R <rmanjunath7692@gmail.com>
2023-03-21 14:46:24 +0530

    update

---
+++ 1-variables, datatypes/challenge2.js
@@ -0,0 +1,14 @@
+// create a variable for the sum, difference, product,qoutient and remainder of x and y. log the output in a string contianing the two numbers x and y along with the result
+//
+// 
+// your code here:
+
+
+let x=Math.floor(Math.random()*100+1)
+let y=Math.floor(Math.random()*100+1)
+
+let sum=console.log(`sum of ${x} + ${y} is: `+(x+y));
+let diff=console.log(`diff of ${x} - ${y} is: `+(x-y));
+let pro=console.log(`mul of ${x} * ${y} is: `+(x*y));
+let quo=console.log(`div of ${x} / ${y} is: `+(x/y));
+let rem=console.log(`rem of ${x} % ${y} is: `+(x%y)); 
